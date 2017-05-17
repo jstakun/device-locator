f8d3c51e9c442b72ef0e48179b6a035ffbfbb632
jstakun <jstakun@gms-world.net>
2017-05-17 20:56:47 +0200

    device locator initial commit

---
+++ app/src/main/java/com/gmsworld/devicelocator/BroadcastReceivers/SmsReceiver.java
@@ -0,0 +1,317 @@
+package com.gmsworld.devicelocator.BroadcastReceivers;
+
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.media.AudioManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.ActivityCompat;
+import android.telephony.SmsMessage;
+import android.util.Log;
+import android.widget.Toast;
+
+import net.gmsworld.locatedriver.R;
+import com.gmsworld.devicelocator.Services.GmsLocationServicesManager;
+import com.gmsworld.devicelocator.Services.RouteTrackingService;
+import com.gmsworld.devicelocator.Services.SmsSenderService;
+import com.gmsworld.devicelocator.Utilities.Network;
+import com.gmsworld.devicelocator.Utilities.Permissions;
+import com.gmsworld.devicelocator.Utilities.RouteTrackingServiceUtils;
+
+import java.util.ArrayList;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class SmsReceiver extends BroadcastReceiver {
+
+    private final static String TAG = SmsReceiver.class.getSimpleName();
+
+    public final static String START_COMMAND = "startdl"; //start route tracking
+    public final static String STOP_COMMAND = "stopdl"; //stop route tracking
+    public final static String ROUTE_COMMAND = "routedl"; //share currently recorded route
+    public final static String MUTE_COMMAND = "mutedl"; //mute phone
+    public final static String NORMAL_COMMAND = "normaldl"; //unmute phone
+    public final static String SHARE_COMMAND = "locatedl"; //share current location
+    public final static String RADIUS_COMMAND = "radiusdl"; //change tracking radius, usage radiusdl x where is number of meters > 0
+    public final static String CALL_COMMAND = "calldl"; //call to sender
+    public final static String GPS_HIGH_COMMAND = "gpshighdl"; //set high gps accuracy
+    public final static String GPS_BALANCED_COMMAND = "gpsbalancedl"; //set balanced gps accuracy
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        findKeyword(context, intent);
+        findStartRouteTrackerServiceStartCommand(context, intent);
+        findStopRouteTrackerServiceStartCommand(context, intent);
+        findMuteCommand(context, intent);
+        findNormalCommand(context, intent);
+        findChangeRadiusRouteTrackerServiceCommand(context, intent);
+        findStartPhoneCallCommand(context, intent);
+        findShareRouteCommand(context, intent);
+        findGpsHighAccuracyCommand(context, intent);
+        findGpsLowAccuracyCommand(context, intent);
+    }
+
+    private void findStartRouteTrackerServiceStartCommand(Context context, Intent intent) {
+        String sender = getSenderAddress(context, intent, START_COMMAND);
+
+        if (sender != null) {
+            SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
+            int radius = settings.getInt("radius", RouteTrackingService.DEFAULT_RADIUS);
+            String phoneNumber = settings.getString("phoneNumber", "");
+            String email = settings.getString("email", "");
+            String telegramId = settings.getString("telegramId", "");
+
+            RouteTrackingServiceUtils.startRouteTrackingService(context, null, radius, phoneNumber, email, telegramId, false);
+
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putBoolean("motionDetectorRunning", true);
+            editor.commit();
+
+            Intent newIntent = new Intent(context, SmsSenderService.class);
+            newIntent.putExtra("phoneNumber", sender);
+            newIntent.putExtra("command", START_COMMAND);
+            context.startService(newIntent);
+        }
+    }
+
+    private void findStopRouteTrackerServiceStartCommand(Context context, Intent intent) {
+        String sender = getSenderAddress(context, intent, STOP_COMMAND);
+
+        if (sender != null) {
+            RouteTrackingServiceUtils.stopRouteTrackingService(context, null, false);
+
+            SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putBoolean("motionDetectorRunning", false);
+            editor.commit();
+
+            Intent newIntent = new Intent(context, SmsSenderService.class);
+            newIntent.putExtra("phoneNumber", sender);
+            newIntent.putExtra("command", STOP_COMMAND);
+            context.startService(newIntent);
+        }
+    }
+
+    private void findChangeRadiusRouteTrackerServiceCommand(Context context, Intent intent) {
+        ArrayList<SmsMessage> list = null;
+        try {
+            String keyword = RADIUS_COMMAND;
+            String token = PreferenceManager.getDefaultSharedPreferences(context).getString("token", "");
+            keyword += token;
+            list = getMessagesWithKeyword(keyword, intent.getExtras());
+        } catch (Exception e) {
+            return;
+        }
+
+        if (list.size() > 0) {
+            int radius = -1;
+            String[] tokens = list.get(0).getMessageBody().split(" ");
+            if (tokens.length == 2) {
+                try {
+                    radius = Integer.parseInt(tokens[1]);
+                } catch (Exception e) {
+                    Log.e(TAG, "Wrong radius: " + tokens[1]);
+                }
+            }
+            if (radius > 0) {
+                SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
+                String phoneNumber = settings.getString("phoneNumber", "");
+                String email = settings.getString("email", "");
+                String telegramId = settings.getString("telegramId", "");
+
+                RouteTrackingServiceUtils.resetRouteTrackingService(context, null, false, radius, phoneNumber, email, telegramId);
+
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putInt("radius", radius);
+                editor.commit();
+
+                Intent newIntent = new Intent(context, SmsSenderService.class);
+                newIntent.putExtra("phoneNumber", list.get(0).getOriginatingAddress());
+                newIntent.putExtra("command", RADIUS_COMMAND);
+                context.startService(newIntent);
+            }
+        }
+    }
+
+    private void findStartPhoneCallCommand(Context context, Intent intent) {
+        String sender = getSenderAddress(context, intent, CALL_COMMAND);
+
+        if (sender != null) {
+            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            Uri call = Uri.parse("tel:" + sender);
+            Intent surf = new Intent(Intent.ACTION_CALL, call);
+            //surf.setPackage("com.android.phone"); //use default phone
+            //surf.setPackage("com.google.android.dialer");
+            surf.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(surf);
+        }
+    }
+
+    private void findShareRouteCommand(Context context, Intent intent) {
+        String sender = getSenderAddress(context, intent, ROUTE_COMMAND);
+
+        if (sender != null) {
+            Intent routeTracingService = new Intent(context, RouteTrackingService.class);
+            routeTracingService.putExtra(RouteTrackingService.COMMAND, RouteTrackingService.COMMAND_ROUTE);
+
+            SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
+            String title = settings.getString("routeTitle", "");
+
+            if (title == null || title.length() == 0) {
+                title = "devicelocatorroute_" + Network.getDeviceId(context) + "_" + System.currentTimeMillis();
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putString("routeTitle", title);
+                editor.commit();
+            }
+
+            routeTracingService.putExtra("title", title);
+            context.startService(routeTracingService);
+
+            Intent newIntent = new Intent(context, SmsSenderService.class);
+            newIntent.putExtra("phoneNumber", sender);
+            newIntent.putExtra("command", ROUTE_COMMAND);
+            newIntent.putExtra("title", title);
+            newIntent.putExtra("size", GmsLocationServicesManager.getInstance().getRouteSize());
+            context.startService(newIntent);
+        }
+    }
+
+    private void findMuteCommand(Context context, Intent intent) {
+        String sender = getSenderAddress(context, intent, MUTE_COMMAND);
+
+        if (sender != null) {
+            final AudioManager audioMode = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+            audioMode.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+
+            Intent newIntent = new Intent(context, SmsSenderService.class);
+            newIntent.putExtra("phoneNumber", sender);
+            newIntent.putExtra("command", MUTE_COMMAND);
+            context.startService(newIntent);
+        }
+    }
+
+    private void findNormalCommand(Context context, Intent intent) {
+        String sender = getSenderAddress(context, intent, NORMAL_COMMAND);
+
+        if (sender != null) {
+            final AudioManager audioMode = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+            audioMode.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+
+            Intent newIntent = new Intent(context, SmsSenderService.class);
+            newIntent.putExtra("phoneNumber", sender);
+            newIntent.putExtra("command", NORMAL_COMMAND);
+            context.startService(newIntent);
+        }
+    }
+
+    private void findKeyword(Context context, Intent intent) {
+        String keyword = PreferenceManager.getDefaultSharedPreferences(context).getString("keyword", "");
+
+        if (keyword.length() == 0) {
+            keyword = SHARE_COMMAND;
+        }
+
+        String token = PreferenceManager.getDefaultSharedPreferences(context).getString("token", "");
+        keyword += token;
+
+        ArrayList<SmsMessage> list = null;
+        try {
+            list = getMessagesWithKeyword(keyword, intent.getExtras());
+        } catch (Exception e) {
+            return;
+        }
+
+        if (list.size() > 0) {
+            if (!Permissions.haveSendSMSAndLocationPermission(context)) {
+                try {
+                    Permissions.setPermissionNotification(context);
+                } catch (Exception e) {
+                    Toast.makeText(context, R.string.send_sms_and_location_permission, Toast.LENGTH_SHORT).show();
+                }
+
+                return;
+            }
+
+            Intent newIntent = new Intent(context, SmsSenderService.class);
+            newIntent.putExtra("phoneNumber", list.get(0).getOriginatingAddress());
+            context.startService(newIntent);
+        }
+    }
+
+    private void findGpsHighAccuracyCommand(Context context, Intent intent) {
+        String sender = getSenderAddress(context, intent, GPS_HIGH_COMMAND);
+
+        if (sender != null) {
+            SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putInt("gpsAccuracy", 1);
+            editor.commit();
+            RouteTrackingServiceUtils.setHighGpsAccuracy(context);
+            Intent newIntent = new Intent(context, SmsSenderService.class);
+            newIntent.putExtra("phoneNumber", sender);
+            newIntent.putExtra("command", GPS_HIGH_COMMAND);
+            context.startService(newIntent);
+        }
+    }
+
+    private void findGpsLowAccuracyCommand(Context context, Intent intent) {
+        String sender = getSenderAddress(context, intent, GPS_BALANCED_COMMAND);
+
+        if (sender != null) {
+            SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putInt("gpsAccuracy", 0);
+            editor.commit();
+            RouteTrackingServiceUtils.setBalancedGpsAccuracy(context);
+            Intent newIntent = new Intent(context, SmsSenderService.class);
+            newIntent.putExtra("phoneNumber", sender);
+            newIntent.putExtra("command", GPS_BALANCED_COMMAND);
+            context.startService(newIntent);
+        }
+    }
+
+    private ArrayList<SmsMessage> getMessagesWithKeyword(String keyword, Bundle bundle) {
+        ArrayList<SmsMessage> list = new ArrayList<SmsMessage>();
+        if (bundle != null) {
+            Object[] pdus = (Object[]) bundle.get("pdus");
+            for (int i = 0; i < pdus.length; i++) {
+                SmsMessage sms = null;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    String format = bundle.getString("format");
+                    sms = SmsMessage.createFromPdu((byte[]) pdus[i], format);
+                } else {
+                    sms = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                }
+
+                if (StringUtils.equalsIgnoreCase(sms.getMessageBody(), keyword)) {
+                    list.add(sms);
+                } else if (keyword.equals(RADIUS_COMMAND) && StringUtils.startsWithIgnoreCase(sms.getMessageBody(), keyword)) {
+                    list.add(sms);
+                }
+            }
+        }
+        return list;
+    }
+
+    private String getSenderAddress(Context context, Intent intent, String command) {
+        try {
+            String token = PreferenceManager.getDefaultSharedPreferences(context).getString("token", "");
+            command += token;
+            ArrayList<SmsMessage> list = getMessagesWithKeyword(command, intent.getExtras());
+            if (list.size() > 0) {
+                return list.get(0).getOriginatingAddress();
+            }
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage(), e);
+        }
+        return null;
+    }
+}
---
+++ app/src/main/java/com/gmsworld/devicelocator/Utilities/Network.java
@@ -0,0 +1,136 @@
+package com.gmsworld.devicelocator.Utilities;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.provider.Settings;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Network {
+
+    private static final String TAG = Network.class.getSimpleName();
+
+    private static final String FORM_ENCODING = "application/x-www-form-urlencoded;charset=UTF-8";
+
+    protected static boolean isNetworkAvailable(final Context context) {
+        final ConnectivityManager connectivityManager = ((ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE));
+        return connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected();
+    }
+
+    protected static void get(final String urlString, final OnGetFinishListener onGetFinishListener) {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL(urlString);
+                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                    InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+
+                    BufferedReader r = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder total = new StringBuilder();
+                    String line;
+                    while ((line = r.readLine()) != null) {
+                        total.append(line).append('\n');
+                    }
+
+                    onGetFinishListener.onGetFinish(total.toString());
+                } catch (Exception e) {
+                    onGetFinishListener.onGetFinish("{}");
+                }
+            }
+        };
+
+        thread.start();
+    }
+
+    public static void post(final String urlString, final String content, final String contentType, final OnGetFinishListener onGetFinishListener) {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL(urlString);
+                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                    urlConnection.setRequestMethod("POST");
+
+                    if (content != null) {
+                        urlConnection.setRequestProperty("Content-Length", Integer.toString(content.length()));
+
+                        if (contentType != null) {
+                            urlConnection.setRequestProperty("Content-Type", contentType);
+                        } else {
+                            urlConnection.setRequestProperty("Content-Type", FORM_ENCODING);
+                        }
+
+                        urlConnection.setDoInput(true);
+                        urlConnection.setDoOutput(true);
+
+                        //Log.d(TAG, "Sending post: " + content);
+
+                        //Send request
+                        DataOutputStream wr = new DataOutputStream(urlConnection.getOutputStream());
+                        wr.writeBytes(content);
+                        wr.flush();
+                        wr.close();
+                    } else {
+                        urlConnection.connect();
+                    }
+
+                    int responseCode = urlConnection.getResponseCode();
+
+                    onGetFinishListener.onGetFinish("{\"responseCode\": " + responseCode + "}");
+                } catch (Exception e) {
+                    onGetFinishListener.onGetFinish("{}");
+                }
+            }
+        };
+
+        thread.start();
+    }
+
+    public static void execute(Runnable r) {
+        Thread thread = new Thread(r);
+        thread.start();
+    }
+
+    public static String getDeviceId(Context context) {
+        String androidDeviceId = null;
+
+        if (context != null) {
+            //android.Manifest.permission.READ_PHONE_STATE required
+            // get telephony imei
+            try {
+                final TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+                androidDeviceId = tm.getDeviceId(); //imei
+            } catch (Exception e) {
+                Log.e(TAG, e.getMessage(), e);
+            }
+
+            // get internal android device id
+            if (androidDeviceId == null || androidDeviceId.length() == 0) {
+                try {
+                    androidDeviceId = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
+                } catch (Exception e) {
+                    Log.e(TAG, e.getMessage(), e);
+                }
+            }
+        }
+
+        if (androidDeviceId == null) {
+            androidDeviceId = "unknown";
+        }
+
+        return androidDeviceId;
+    }
+
+    public interface OnGetFinishListener {
+        public void onGetFinish(String result);
+    }
+}
