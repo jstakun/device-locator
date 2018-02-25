ddc2e21a7d78b067e83be4035c5a5b50a807acd6
jstakun <jstakun@gms-world.net>
2018-02-25 15:11:54 +0100

    v14

--- app/src/main/java/net/gmsworld/devicelocator/Utilities/Files.java
+++ app/src/main/java/net/gmsworld/devicelocator/Utilities/Files.java
@@ -22,35 +22,7 @@ public class Files {
     public static List<String> readFileByLinesFromContextDir(String filename, Context context) {
         List<String> lines = new ArrayList<String>();
         QueueFile queueFile = null;
-        //v1
-        /*InputStream is = null;
 
-        try {
-            File fc = new File(context.getFilesDir(), filename);
-            if (fc.exists()) {
-                is = new FileInputStream(fc);
-                long start = System.nanoTime();
-                BufferedReader in = new BufferedReader(new InputStreamReader(is));
-                String line;
-                while ((line = in.readLine()) != null) {
-                    lines.add(line);
-                }
-                long time = System.nanoTime() - start;
-                Log.d(TAG, "Route file processed in " + time + " ns.");
-            }
-        } catch (Exception e) {
-            Log.d(TAG, e.getMessage(), e);
-        } finally {
-            if (is != null) {
-                try {
-                    is.close();
-                } catch (Exception e) {
-                    Log.d(TAG, e.getMessage(), e);
-                }
-            }
-        }*/
-
-        //v2
         try {
             File fc = new File(context.getFilesDir(), filename);
             if (fc.exists()) {
@@ -82,35 +54,7 @@ public class Files {
     public static boolean hasRoutePoints(String filename, Context context, int numOfPoints) {
         int i=0;
         QueueFile queueFile = null;
-        //v1
-        /*InputStream is = null;
 
-        try {
-            File fc = new File(context.getFilesDir(), filename);
-            if (fc.exists()) {
-                is = new FileInputStream(fc);
-                BufferedReader in = new BufferedReader(new InputStreamReader(is));
-                while (i < numOfPoints) {
-                    String line = in.readLine();
-                    if (line == null) {
-                        break;
-                    } else {
-                        i += 1;
-                    }
-                }
-            }
-        } catch (Exception e) {
-            Log.d(TAG, e.getMessage(), e);
-        } finally {
-            if (is != null) {
-                try {
-                    is.close();
-                } catch (Exception e) {
-                    Log.d(TAG, e.getMessage(), e);
-                }
-            }
-        }*/
-        //v2
         try {
             File fc = new File(context.getFilesDir(), filename);
             if (fc.exists()) {
@@ -129,6 +73,7 @@ public class Files {
                 }
             }
         }
+
         return (i >= numOfPoints);
     }
 
@@ -143,25 +88,8 @@ public class Files {
     }
 
     public static void appendLineToFileFromContextDir(String filename, Context context, String line) {
-        //v1
-        /*FileOutputStream outputStream = null;
-        try {
-            line += "\n";
-            outputStream = context.openFileOutput(filename, Context.MODE_APPEND);
-            outputStream.write(line.getBytes());
-        } catch (Exception e) {
-            Log.d(TAG, e.getMessage(), e);
-        } finally {
-            if (outputStream != null) {
-                try {
-                    outputStream.close();
-                } catch (Exception e) {
-                    Log.d(TAG, e.getMessage(), e);
-                }
-            }
-        }*/
-        //v2
         QueueFile queueFile = null;
+
         try {
             File fc = new File(context.getFilesDir(), filename);
             queueFile = new QueueFile.Builder(fc).build();
