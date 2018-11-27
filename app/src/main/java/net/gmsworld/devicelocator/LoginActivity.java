8326d980ccb6f3549ae6d19f513777d13b085602
jstakun <jstakun@gms-world.net>
2018-11-27 15:09:51 +0100

    password reset

--- app/src/main/java/net/gmsworld/devicelocator/LoginActivity.java
+++ app/src/main/java/net/gmsworld/devicelocator/LoginActivity.java
@@ -5,9 +5,11 @@ import android.accounts.AccountManager;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.res.ColorStateList;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
@@ -26,6 +28,7 @@ import android.widget.Toast;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 
+import net.gmsworld.devicelocator.utilities.Messenger;
 import net.gmsworld.devicelocator.utilities.Network;
 import net.gmsworld.devicelocator.utilities.SCUtils;
 
@@ -82,6 +85,9 @@ public class LoginActivity extends AppCompatActivity  {
         TextView register = findViewById(R.id.register);
         register.setText(Html.fromHtml(getString(R.string.registerLink)));
         register.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView reset = findViewById(R.id.reset);
+        reset.setText(Html.fromHtml(getString(R.string.resetLink)));
     }
 
     private void attemptLogin() {
@@ -132,6 +138,74 @@ public class LoginActivity extends AppCompatActivity  {
             } else {
                 Toast.makeText(this, R.string.no_network_error, Toast.LENGTH_LONG).show();
             }
+        }
+    }
+
+    public void resetPassword(final View v) {
+        String email = mEmailView.getText().toString();
+        if (StringUtils.isNotEmpty(email)) {
+            if (Network.isNetworkAvailable(this)) {
+                Toast.makeText(this, R.string.please_wait, Toast.LENGTH_LONG).show();
+                final SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
+                String tokenStr = settings.getString(DeviceLocatorApp.GMS_TOKEN, "");
+                if (StringUtils.isNotEmpty(tokenStr)) {
+                    Map<String, String> headers = new HashMap<String, String>();
+                    headers.put("Authorization", "Bearer " + tokenStr);
+                    headers.put("X-GMS-AppId", "2");
+                    String deviceId = Messenger.getDeviceId(this, false);
+                    if (StringUtils.isNotEmpty(deviceId)) {
+                        headers.put("X-GMS-DeviceId", deviceId);
+                    }
+                    headers.put("X-GMS-UseCount", Integer.toString(settings.getInt("useCount", 1)));
+                    String queryString = "type=reset&login=" + email;
+                    Network.post(this, getString(R.string.notificationUrl), queryString, null, headers, new Network.OnGetFinishListener() {
+                        @Override
+                        public void onGetFinish(String results, int responseCode, String url) {
+                            if (responseCode == 200) {
+                                JsonElement reply = new JsonParser().parse(results);
+                                String status = null;
+                                if (reply != null) {
+                                    JsonElement st = reply.getAsJsonObject().get("status");
+                                    if (st != null) {
+                                        status = st.getAsString();
+                                    }
+                                }
+                                if (StringUtils.equals(status, "ok")) {
+                                    Toast.makeText(LoginActivity.this, "Please check your email inbox for password reset instructions", Toast.LENGTH_LONG).show();
+                                } else {
+                                    Toast.makeText(LoginActivity.this, R.string.internal_error, Toast.LENGTH_LONG).show();
+                                }
+                            } else if (responseCode == 400) {
+                                Toast.makeText(LoginActivity.this, "Failed to reset password. Is your login valid?", Toast.LENGTH_LONG).show();
+                            } else {
+                                Toast.makeText(LoginActivity.this, R.string.internal_error, Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+                } else {
+                    String queryString = "scope=dl&user=" + Messenger.getDeviceId(this, false);
+                    Network.get(this, getString(R.string.tokenUrl) + "?" + queryString, null, new Network.OnGetFinishListener() {
+                        @Override
+                        public void onGetFinish(String results, int responseCode, String url) {
+                            if (responseCode == 200) {
+                                if (StringUtils.isNotEmpty(Messenger.getToken(LoginActivity.this, results))) {
+                                    resetPassword(v);
+                                } else {
+                                    Toast.makeText(LoginActivity.this, R.string.internal_error, Toast.LENGTH_LONG).show();
+                                    Log.e(TAG, "Failed to parse token!");
+                                }
+                            } else {
+                                Toast.makeText(LoginActivity.this, R.string.internal_error, Toast.LENGTH_LONG).show();
+                                Log.d(TAG, "Failed to receive token: " + results);
+                            }
+                        }
+                    });
+                }
+            } else {
+                Toast.makeText(this, R.string.no_network_error, Toast.LENGTH_LONG).show();
+            }
+        } else {
+            Toast.makeText(this, "Please enter your login", Toast.LENGTH_LONG).show();
         }
     }
 
