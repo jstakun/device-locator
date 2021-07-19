508ba9d32b334b7dfcd6629bd90ef39b60e20b25
jstakun <jstakun@gms-world.net>
2021-07-19 15:14:40 +0200

    v97

--- app/src/main/java/net/gmsworld/devicelocator/services/CommandService.java
+++ app/src/main/java/net/gmsworld/devicelocator/services/CommandService.java
@@ -224,17 +224,19 @@ public class CommandService extends IntentService implements OnLocationUpdatedLi
                     public void onGetFinish(String results, int responseCode, String url) {
                         if (responseCode == 200) {
                             toaster.showServiceToast(R.string.command_sent_to_device, StringUtils.capitalize(command), deviceName);
-                            commandsSent.add(imei + "_" + command);
+                            final String commandSent = imei + "_" +  command.toLowerCase();
+                            commandsSent.add(commandSent);
                             commandHandler.postDelayed(new Runnable() {
                                 @Override
                                 public void run() {
                                     Log.d(TAG, "Checking if reply to command " + command + " received from the device " + imei);
-                                    if (commandsSent.contains(imei + "_" + command)) {
+                                    if (commandsSent.contains(commandSent)) {
                                         Bundle extras = new Bundle();
                                         extras.putString(MainActivity.DEVICE_NAME, deviceName);
                                         extras.putString("imei", imei);
                                         extras.putString("command", command);
-                                        NotificationUtils.showMessageNotification(CommandService.this, "No reply to command " + StringUtils.capitalize(command) + " received from device " + deviceName + ". Please send this command again.", null ,extras);
+                                        NotificationUtils.showMessageNotification(CommandService.this, "No reply to command " + StringUtils.capitalize(command) + " received from device " + deviceName + ".\nPlease send this command again.", null ,extras);
+                                        commandsSent.remove(commandSent);
                                     }
                                 }
                             }, 180000); //3 mins
@@ -289,8 +291,8 @@ public class CommandService extends IntentService implements OnLocationUpdatedLi
         Files.appendLineToFileFromContextDir(auditFile, auditLog, 100, 10);
     }
 
-    public static void receivedReplyToCommand(String imei, String command) {
+    public static void receivedReplyToCommand(final String imei, final String command) {
         Log.d(TAG, "Received reply to command " + command + " from device " + imei);
-        commandsSent.remove(imei + " " + command);
+        commandsSent.remove(imei + "_" + command.toLowerCase());
     }
 }
